3bc962a51bd0ebabc8284530c210c787e76597ca
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2023-04-25 08:32:42 +0200

    Add another unit test for the Cosmic chronometer datasets with (x=z+1)

--- HEAL.EquationSearch.Test/Test.cs
+++ HEAL.EquationSearch.Test/Test.cs
@@ -145,13 +145,17 @@ namespace HEAL.EquationSearch.Test {
     }
 
     [TestMethod]
-    public void CosmicChronometer() {
-      // TODO: does not work correctly yet
-
+    public void CosmicChronometerZ() {
       // Run for Cosmic Chronometer dataset from Exhaustive Symbolic Regression
       // https://github.com/DeaglanBartlett/ESR/blob/main/esr/data/CC_Hubble.dat
       // https://arxiv.org/pdf/2211.11461.pdf
       var parameters = "--dataset CC_Hubble.csv --target H --inputs z --train 0:31 --max-length 30 --noise-sigma H_err --seed 1234";
+      HEAL.EquationSearch.Console.Program.Main(parameters.Split(" ", StringSplitOptions.RemoveEmptyEntries));
+    }
+    [TestMethod]
+    public void CosmicChronometerX() {
+      // as above but use x = z+1 and search for H(x)
+      var parameters = "--dataset CC_Hubble.csv --target H --inputs x --train 0:31 --max-length 30 --noise-sigma H_err --seed 1234";
       HEAL.EquationSearch.Console.Program.Main(parameters.Split(" ", StringSplitOptions.RemoveEmptyEntries));
     }
   }
