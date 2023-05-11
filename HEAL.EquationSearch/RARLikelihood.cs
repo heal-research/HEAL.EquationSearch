c3e0b2bb615a13c7d604dda21ac3f774fe61d111
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2023-05-11 19:27:06 +0200

    Worked on RAR likelihood function.

--- HEAL.EquationSearch/RARLikelihood.cs
+++ HEAL.EquationSearch/RARLikelihood.cs
@@ -1,6 +1,5 @@
 ﻿using HEAL.Expressions;
 using HEAL.NonlinearRegression;
-using System;
 using System.Linq.Expressions;
 using System.Reflection;
 using LinqExpr = System.Linq.Expressions.Expression;
@@ -8,9 +7,19 @@ using LinqExpr = System.Linq.Expressions.Expression;
 namespace HEAL.EquationSearch {
 
   // as reported in https://arxiv.org/pdf/2301.04368.pdf Eq. 3
+  // additional info from Harry:
+  // e_loggbar = e_gbar / (gbar * np.log(10.))
+  // e_loggobs = e_gobs / (gobs * np.log(10.))
+  // sigma2_tot = e_loggobs**2 + (gobs1_diff*e_loggbar)**2
+  // negloglike = 0.5 * np.sum((np.log10(gobs) - np.log10(gobs1))**2 ./ sigma2_tot + np.log(2.* np.pi * sigma2_tot))
+
   public class RARLikelihood : LikelihoodBase {
     private readonly double[] e_log_gobs;
     private readonly double[] e_log_gbar;
+    private readonly double[][] extendedXCol;
+    private int y_idx;
+    private int e_log_gbar_idx;
+    private int e_log_gobs_idx;
 
     private readonly static MethodInfo log = typeof(Math).GetMethod("Log", new Type[] { typeof(double) });
     private readonly static MethodInfo abs = typeof(Math).GetMethod("Abs", new Type[] { typeof(double) });
@@ -18,38 +27,32 @@ namespace HEAL.EquationSearch {
 
 
     private Expression<Expr.ParametricFunction> origExpr;
-    // private ExpressionInterpreter df_dgbar_Interpreter;
-    // private Expression<Func<double[], double[], double, double, double>> likelihoodExpr;
     private ExpressionInterpreter likelihoodInterpreter = null;
     private ExpressionInterpreter bestLikelihoodInterpreter = null;
     private ExpressionInterpreter[] likelihoodGradInterpreter = null;
+
+    private double[,] jacP; // buffer for jacobian
 
     public override Expression<Expr.ParametricFunction> ModelExpr {
       get => origExpr;
       set {
         origExpr = value;
-
-        // to simplify calculation of Jac and Hess we create a parametric expression for the likelihood of one observation by wrapping the model
-        // df_dgbar_Interpreter = null;
-        // likelihoodExpr = null;
-
+        this.jacP = null;
         if (value != null) {
+          // We create an expression for the likelihood which wraps the model expression as well as it's partial derivative over log(bar).
+          // This allows us to use the autodiff interpreter for the full likelihood and simplifies the likelihood code.
+
+          var numParam = Expr.NumberOfParameters(origExpr);
+
           var pParam = value.Parameters[0];
           var xParam = value.Parameters[1];
 
-          // the interpreter for the likelihood has additional variables e_log_gbar, e_log_gobs, and y
-          var extendedXCol = new double[xCol.Length + 3][];
-          for (int i = 0; i < xCol.Length; i++) {
-            extendedXCol[i] = xCol[i];
-          }
-
-          extendedXCol[xCol.Length] = e_log_gobs; var e_log_gobs_idx = xCol.Length;
-          extendedXCol[xCol.Length + 1] = e_log_gbar; var e_log_gbar_idx = xCol.Length + 1;
-          extendedXCol[xCol.Length + 2] = y.Select(Math.Log10).ToArray(); var y_idx = xCol.Length + 2;
+          this.jacP = new double[y.Length, numParam];
 
           // wrap log(f(x))
           value = value.Update(LinqExpr.Divide(
-            LinqExpr.Call(log, LinqExpr.Call(abs, value.Body)),
+            // LinqExpr.Call(log, LinqExpr.Call(abs, value.Body)),
+            LinqExpr.Call(log, value.Body),
             LinqExpr.Constant(Math.Log(10))), value.Parameters);
 
           var d_log_f_dgbar = Expr.Derive(value, value.Parameters[1], 0);
@@ -62,29 +65,29 @@ namespace HEAL.EquationSearch {
             LinqExpr.Multiply(e_log_gobs_expr, e_log_gobs_expr),
             LinqExpr.Call(pow, LinqExpr.Multiply(LinqExpr.Multiply(LinqExpr.Multiply(d_log_f_dgbar.Body, gbar_expr), LinqExpr.Constant(Math.Log(10))), e_log_gbar_expr), LinqExpr.Constant(2.0))
             );
-          // df_dgbar_Interpreter = new ExpressionInterpreter(d_log_f_dgbar, xCol);
 
           var baseExpr = LinqExpr.Multiply(LinqExpr.Constant(0.5), LinqExpr.Call(log, LinqExpr.Multiply(LinqExpr.Constant(2.0 * Math.PI), sTotExpr))); //  0.5 * Math.Log(2.0 * Math.PI * stot)
           var residualExpr = LinqExpr.Multiply(LinqExpr.Constant(0.5), LinqExpr.Divide(LinqExpr.Call(pow, LinqExpr.Subtract(value.Body, y_expr), LinqExpr.Constant(2.0)), sTotExpr)); //0.5 * res * res / stot;
 
           Expression<Expr.ParametricFunction> likelihoodExpr = LinqExpr.Lambda<Expr.ParametricFunction>(
-            body: LinqExpr.Add(residualExpr, baseExpr), 
+            body: LinqExpr.Add(residualExpr, baseExpr),
             pParam, xParam);
-          
+
           Expression<Expr.ParametricFunction> baseLikelihoodExpr = LinqExpr.Lambda<Expr.ParametricFunction>(
             body: baseExpr,
             pParam, xParam);
 
+
           likelihoodInterpreter = new ExpressionInterpreter(likelihoodExpr, extendedXCol);
           bestLikelihoodInterpreter = new ExpressionInterpreter(baseLikelihoodExpr, extendedXCol);
 
-          var numParam = Expr.NumberOfParameters(origExpr);
           likelihoodGradInterpreter = new ExpressionInterpreter[numParam];
-          for(int i=0;i< numParam; i++) {
+          for (int i = 0; i < numParam; i++) {
             var dLikeExpr = Expr.Derive(likelihoodExpr, i);
             likelihoodGradInterpreter[i] = new ExpressionInterpreter(dLikeExpr, extendedXCol);
           }
         }
+
         base.ModelExpr = value;
       }
     }
@@ -93,30 +96,42 @@ namespace HEAL.EquationSearch {
     internal RARLikelihood(RARLikelihood original) : base(original) {
       this.e_log_gobs = original.e_log_gobs;
       this.e_log_gbar = original.e_log_gbar;
-      this.likelihoodInterpreter = original.likelihoodInterpreter;
-      this.bestLikelihoodInterpreter = original.bestLikelihoodInterpreter;
-      this.likelihoodGradInterpreter = original.likelihoodGradInterpreter;
-
+      this.extendedXCol = original.extendedXCol;
+      this.y_idx = original.y_idx;
+      this.e_log_gbar_idx = original.e_log_gbar_idx;
+      this.e_log_gobs_idx = original.e_log_gobs_idx;
+      ModelExpr = original.origExpr; // initializes all interpreters and gradients
     }
+
     public RARLikelihood(double[,] x, double[] y, Expression<Expr.ParametricFunction> modelExpr, double[] e_log_gobs, double[] e_log_gbar)
       : base(modelExpr, x, y, 0) {
       this.e_log_gobs = e_log_gobs;
       this.e_log_gbar = e_log_gbar;
+
+
+      // the interpreter for the likelihood has additional variables e_log_gbar, e_log_gobs, and y
+      this.extendedXCol = new double[xCol.Length + 3][];
+      for (int i = 0; i < xCol.Length; i++) {
+        extendedXCol[i] = xCol[i];
+      }
+
+      extendedXCol[xCol.Length] = e_log_gobs; this.e_log_gobs_idx = xCol.Length;
+      extendedXCol[xCol.Length + 1] = e_log_gbar; this.e_log_gbar_idx = xCol.Length + 1;
+      extendedXCol[xCol.Length + 2] = y.Select(Math.Log10).ToArray(); this.y_idx = xCol.Length + 2;
     }
 
     public override double[,] FisherInformation(double[] p) {
       var m = y.Length;
       var n = p.Length;
-      var likeHessPart = new double[m, n]; // buffer
 
       // FIM is the negative of the second derivative (Hessian) of the log-likelihood
       // -> FIM is the Hessian of the negative log-likelihood
       var hessian = new double[n, n];
       for (int j = 0; j < n; j++) {
-        likelihoodGradInterpreter[j].EvaluateWithJac(p, null, likeHessPart);
+        likelihoodGradInterpreter[j].EvaluateWithJac(p, null, jacP);
         for (int i = 0; i < m; i++) {
           for (int k = 0; k < n; k++) {
-            hessian[j, k] += likeHessPart[i, k];
+            hessian[j, k] += jacP[i, k];
           }
         }
       }
@@ -137,14 +152,11 @@ namespace HEAL.EquationSearch {
     public override void NegLogLikelihoodGradient(double[] p, out double nll, double[]? nll_grad) {
       var m = y.Length;
       var n = p.Length;
-      double[,]? jacP = null;
-
 
       if (nll_grad != null) {
-        jacP = new double[m, n];
-        Array.Clear(nll_grad, 0, n);
+        Array.Clear(nll_grad);
 
-        var nllArr = likelihoodInterpreter.EvaluateWithJac(p, null, jacP);
+        var nllArr = NegLogLikelihoodJacobian(p, jacP);
         nll = nllArr.Sum();
         for (int i = 0; i < m; i++) {
           for (int j = 0; j < n; j++) {
@@ -152,8 +164,16 @@ namespace HEAL.EquationSearch {
           }
         }
       } else {
-        nll = likelihoodInterpreter.Evaluate(p).Sum();
+        nll = NegLogLikelihoodJacobian(p, null).Sum();
       }
+
+      if (double.IsNaN(nll)) {
+        nll = 1e300;
+      }
+    }
+
+    public double[] NegLogLikelihoodJacobian(double[] p, double[,]? jac) {
+      return likelihoodInterpreter.EvaluateWithJac(p, null, jac);
     }
 
     public override LikelihoodBase Clone() {
