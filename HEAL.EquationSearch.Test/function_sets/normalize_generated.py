663a0503f050f7909ca56aa3044b41d507ceafd3
Lukas Kammerer <lukas.kammere@fh-hagenberg.at>
2023-05-30 18:06:46 +0200

    Refactor normalization of generated sentences: Directly edit symbols in sympy structures.

--- HEAL.EquationSearch.Test/function_sets/normalize_generated.py
+++ HEAL.EquationSearch.Test/function_sets/normalize_generated.py
@@ -1,8 +1,9 @@
 from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
 
+import itertools
 import sys
-import re
+
 
 param = symbols("p")
 line_cnt = 1
@@ -11,12 +12,20 @@ line_cnt = 1
 def parse_line(line):
     f = parse_expr(line)
 
+    for func in itertools.chain(f.atoms(Add), f.atoms(Mul), f.atoms(Pow)):
+        is_sqrt = func.is_Pow and func.exp == S.Half
+        is_discrete_exp = func.is_Pow and func.exp.is_Integer
+
+        if not is_sqrt and not is_discrete_exp:
+            for arg in func.args:
+                if arg.is_Integer or arg.is_Rational:
+                    f = f.subs(arg, param)
+
+
     return str(f)
 
 
 def normalize_file(src_filename, target_filename):
-    regex_discrete_multiplication = re.compile(r"\d+\*(?!\*)")
-
     with open(src_filename) as f_src, open(target_filename, "w") as f_out:
         line_cnt = 1
         
@@ -24,7 +33,6 @@ def normalize_file(src_filename, target_filename):
         for line in f_src:
             try:
                 parsed_line = parse_line(line)
-                parsed_line = regex_discrete_multiplication.sub("p*", parsed_line)
 
                 if "-" not in parsed_line and parsed_line not in functions:
                   f_out.write(str(parsed_line) + "\n")
