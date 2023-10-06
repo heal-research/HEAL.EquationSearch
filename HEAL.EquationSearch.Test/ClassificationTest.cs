8840f4b99b8fc06da2f3f6b2c74907837f56f60f
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2023-10-06 15:23:27 +0200

    Added another dataset (to be modelled using a negative binomial / Poisson distribution).

--- HEAL.EquationSearch.Test/ClassificationTest.cs
+++ HEAL.EquationSearch.Test/ClassificationTest.cs
@@ -12,14 +12,14 @@ namespace HEAL.EquationSearch.Test {
 
     [DataTestMethod]
     [DataRow(20)]
-    public void BreadthFirstSearchReducedGrammar(int maxLength) {
+    public void BankruptcyBreadthFirstSearchReducedGrammar(int maxLength) {
       var options = new HEAL.EquationSearch.Console.Program.RunOptions();
       options.Dataset = "bankruptcy.csv";
       options.Target = "Bankrupt";
       options.TrainingRange = "0:48";
       options.Seed = 1234;
 
-      GetData(options, out var inputs, out var trainX, out var trainY);
+      GetBankruptcyData(options, out var inputs, out var trainX, out var trainY);
 
       var grammar = new Grammar(inputs, maxLength);
       grammar.UseLogExpPowRestrictedRules();
@@ -35,8 +35,42 @@ namespace HEAL.EquationSearch.Test {
       // Assert.AreEqual(expectedDL, alg.BestDescriptionLength.Value, Math.Abs(expectedDL * 1e-4));
     }
 
-    private static void GetData(Console.Program.RunOptions options, out string[] inputs, out double[,] trainX, out double[] trainY) {
-      inputs = new string[] { "WC/TA","RE/TA","EBIT/TA","S/TA","BVE/BVL" };
+    [DataTestMethod]
+    [DataRow(15)]
+    public void BikesharingBreadthFirstSearchReducedGrammar(int maxLength) {
+      var options = new HEAL.EquationSearch.Console.Program.RunOptions();
+      options.Dataset = "SeoulBikeData.csv";
+      options.Target = "RentedBikeCount";
+      options.TrainingRange = "0:5000";
+      options.Seed = 1234;
+
+      GetBikeRentalData(options, out var inputs, out var trainX, out var trainY);
+
+      var grammar = new Grammar(inputs, maxLength);
+      grammar.UseLogExpPowRestrictedRules();
+
+      var alg = new Algorithm();
+      var likelihood = new SimpleGaussianLikelihood(trainX, trainY, modelExpr: null);
+      // var evaluator = new Evaluator(likelihood);
+      var evaluator = new VarProEvaluator();
+      alg.Fit(trainX, trainY, noiseSigma: 1.0, inputs, CancellationToken.None, grammar: grammar, evaluator: evaluator, maxLength: maxLength, randSeed: 1234, algorithmType: AlgorithmTypeEnum.BreadthFirst);
+
+      likelihood.ModelExpr = HEALExpressionBridge.ConvertToExpressionTree(alg.BestExpression, alg.VariableNames, out var bestParam);
+      System.Console.WriteLine($"Parameters {string.Join(" ", bestParam.Select(p => p.ToString("e4")))}");
+      System.Console.WriteLine($"{alg.BestExpression.ToInfixString()} DL: {alg.BestDescriptionLength:f2} logL {likelihood.NegLogLikelihood(bestParam):f2}");
+      // Assert.AreEqual(expectedDL, alg.BestDescriptionLength.Value, Math.Abs(expectedDL * 1e-4));
+
+
+    }
+
+    private static void GetBankruptcyData(Console.Program.RunOptions options, out string[] inputs, out double[,] trainX, out double[] trainY) {
+      inputs = new string[] { "WC/TA", "RE/TA", "EBIT/TA", "S/TA", "BVE/BVL" };
+      HEAL.EquationSearch.Console.Program.PrepareData(options, ref inputs, out _, out _, out _, out _, out _, out _, out _, out trainX, out trainY, out _);
+    }
+    private static void GetBikeRentalData(Console.Program.RunOptions options, out string[] inputs, out double[,] trainX, out double[] trainY) {
+      inputs = new string[] { "Date_dec", "date_dec_yearly", "date_dec_weekly", "Hour", "Temperature", "Humidity", "WindSpeed", "Visibility", 
+        "Visibility_2000_or_more", "DewPointTemperature", "SolarRadiation", "Rainfall", "Snowfall", "Seasons_Winter", "Seasons_Summer", "Seasons_Autumn", 
+        "Seasons_Spring", "Holiday", "FunctioningDay" };
       HEAL.EquationSearch.Console.Program.PrepareData(options, ref inputs, out _, out _, out _, out _, out _, out _, out _, out trainX, out trainY, out _);
     }
   }
