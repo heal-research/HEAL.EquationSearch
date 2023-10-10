555fc452b44e91f310c16f2d8671654eb20ca070
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2023-10-10 08:22:22 +0200

    Add poisson likelihood and a test case

---
+++ HEAL.EquationSearch.Test/PoissonLikelihood.cs
@@ -0,0 +1,134 @@
+﻿using HEAL.Expressions;
+using HEAL.NonlinearRegression;
+using System.Linq.Expressions;
+
+namespace HEAL.EquationSearch.Test {
+
+  public class PoissonLikelihood : LikelihoodBase {
+    private double[] yPred; // buffer for model response
+    private double[] nllArr; // buffer for observation likelihoods
+    private double[,] jacP; // buffer for jacobian
+    private double[] logGamma;
+
+
+    internal PoissonLikelihood(PoissonLikelihood original) : base(original) {
+      this.logGamma = (double[])original.logGamma.Clone();
+      yPred = (double[])original.yPred.Clone();
+      nllArr = (double[])original.nllArr.Clone();
+      if (jacP != null)
+        jacP = (double[,])original.jacP.Clone();
+    }
+
+    public override Expression<Expr.ParametricFunction> ModelExpr {
+      get => base.ModelExpr;
+      set {
+        base.ModelExpr = value;
+        if (value != null) {
+
+          var numParam = Expr.NumberOfParameters(value);
+          if (jacP == null || jacP.GetLength(1) != numParam)
+            jacP = new double[NumberOfObservations, numParam];
+        }
+      }
+    }
+
+    public PoissonLikelihood(double[,] x, double[] y, Expression<Expr.ParametricFunction> modelExpr)
+      : base(modelExpr, x, y, 0) {
+      this.logGamma = new double[y.Length];
+      for (int i = 0; i < y.Length; i++) {
+        // prepare logGamma for the evaluation of likelihoods
+        // TODO: lngamma(y+1) ?
+        logGamma[i] = alglib.lngamma(y[i] + 1, out var sgngam); // we ignore sgngam because y is positive
+      }
+
+      // allocate buffers
+      yPred = new double[y.Length];
+      nllArr = new double[y.Length];
+      if (modelExpr != null) {
+        jacP = new double[y.Length, Expr.NumberOfParameters(modelExpr)];
+      }
+    }
+
+    // numeric approximation via the gradient
+    public override double[,] FisherInformation(double[] p) {
+      var n = p.Length;
+      // FIM is the negative of the second derivative (Hessian) of the log-likelihood
+      // -> FIM is the Hessian of the negative log-likelihood
+      var hessian = new double[n, n];
+      var eps = 1e-8;
+      var nll_grad_low = new double[n];
+      var nll_grad_high = new double[n];
+
+      for (int i = 0; i < n; i++) {
+        // numeric approximation (3-point) via gradient
+        var relEps = Math.Max(eps, p[i] * eps);
+        p[i] -= relEps;
+        NegLogLikelihoodGradient(p, out _, nll_grad_low);
+        p[i] += 2 * relEps;
+        NegLogLikelihoodGradient(p, out _, nll_grad_high);
+
+        p[i] -= relEps; // restore
+
+        for (int j = 0; j < n; j++) {
+          hessian[i, j] = (nll_grad_high[j] - nll_grad_low[j]) / (2 * relEps);
+        }
+      }
+
+      // enforce symmetry / smooth out numeric errors
+      for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+          hessian[i, j] = (hessian[i, j] + hessian[j, i]) / 2;
+          hessian[j, i] = hessian[i, j];
+        }
+      }
+
+      return hessian;
+    }
+
+    // for the calculation of deviance
+    public override double BestNegLogLikelihood(double[] p) {
+      var nllSum = 0.0;
+      for (int i = 0; i < y.Length; i++) {
+        nllSum += y[i] * y[i] - Math.Exp(y[i]) - logGamma[i];      // TODO: can be precalculated
+      }
+      return nllSum;
+    }
+
+    public override double NegLogLikelihood(double[] p) {
+      NegLogLikelihoodGradient(p, out var nll, nll_grad: null);
+      return nll;
+    }
+
+    public override void NegLogLikelihoodGradient(double[] p, out double nll, double[] nll_grad = null) {
+      if (nll_grad == null) {
+        NegLogLikelihoodJacobian(p, nllArr, null);
+      } else {
+        NegLogLikelihoodJacobian(p, nllArr, jacP);
+
+        Array.Clear(nll_grad);
+        for (int i = 0; i < jacP.GetLength(0); i++) {
+          for (int j = 0; j < nll_grad.Length; j++) {
+            nll_grad[j] += jacP[i, j];
+          }
+        }
+      }
+      nll = nllArr.Sum();
+    }
+
+    public void NegLogLikelihoodJacobian(double[] p, double[] nll, double[,]? jac) {
+      base.Interpreter.EvaluateWithJac(p, yPred, null, jac); // yPred is log(lambda)
+      for (int i = 0; i < y.Length; i++) {
+        nll[i] = -y[i] * yPred[i] + Math.Exp(yPred[i]) + logGamma[i];
+        if (jac != null) {
+          for (int j = 0; j < p.Length; j++) {
+            jac[i, j] = -y[i] * jac[i, j] + Math.Exp(yPred[i]) * jac[i, j];
+          }
+        }
+      }
+    }
+
+    public override LikelihoodBase Clone() {
+      return new PoissonLikelihood(this);
+    }
+  }
+}
--- HEAL.EquationSearch.Test/PoissonTest.cs
+++ HEAL.EquationSearch.Test/PoissonTest.cs
@@ -2,7 +2,7 @@
 
 namespace HEAL.EquationSearch.Test {
   [TestClass]
-  public class ClassificationTest {
+  public class PoissonTest {
     [TestInitialize]
     public void Init() {
       Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
@@ -11,32 +11,34 @@ namespace HEAL.EquationSearch.Test {
 
 
     [DataTestMethod]
-    [DataRow(20)]
-    public void BankruptcyBreadthFirstSearchReducedGrammar(int maxLength) {
+    [DataRow(10)]
+    public void BikesharingBreadthFirstSearchReducedGrammar(int maxLength) {
       var options = new HEAL.EquationSearch.Console.Program.RunOptions();
-      options.Dataset = "bankruptcy.csv";
-      options.Target = "Bankrupt";
-      options.TrainingRange = "0:48";
+      options.Dataset = "SeoulBikeData.csv";
+      options.Target = "RentedBikeCount";
+      options.TrainingRange = "0:5000";
       options.Seed = 1234;
 
-      GetBankruptcyData(options, out var inputs, out var trainX, out var trainY);
+      GetBikeRentalData(options, out var inputs, out var trainX, out var trainY);
 
       var grammar = new Grammar(inputs, maxLength);
       grammar.UseLogExpPowRestrictedRules();
 
       var alg = new Algorithm();
-      var likelihood = new BernoulliLikelihoodWithLogisticLink(trainX, trainY, modelExpr: null);
+      var likelihood = new PoissonLikelihood(trainX, trainY, modelExpr: null);
       var evaluator = new Evaluator(likelihood);
       alg.Fit(trainX, trainY, noiseSigma: 1.0, inputs, CancellationToken.None, grammar: grammar, evaluator: evaluator, maxLength: maxLength, randSeed: 1234, algorithmType: AlgorithmTypeEnum.BreadthFirst);
-      
+
       likelihood.ModelExpr = HEALExpressionBridge.ConvertToExpressionTree(alg.BestExpression, alg.VariableNames, out var bestParam);
       System.Console.WriteLine($"Parameters {string.Join(" ", bestParam.Select(p => p.ToString("e4")))}");
       System.Console.WriteLine($"{alg.BestExpression.ToInfixString()} DL: {alg.BestDescriptionLength:f2} logL {likelihood.NegLogLikelihood(bestParam):f2}");
       // Assert.AreEqual(expectedDL, alg.BestDescriptionLength.Value, Math.Abs(expectedDL * 1e-4));
     }
 
-    private static void GetBankruptcyData(Console.Program.RunOptions options, out string[] inputs, out double[,] trainX, out double[] trainY) {
-      inputs = new string[] { "WC/TA", "RE/TA", "EBIT/TA", "S/TA", "BVE/BVL" };
+    private static void GetBikeRentalData(Console.Program.RunOptions options, out string[] inputs, out double[,] trainX, out double[] trainY) {
+      inputs = new string[] { "Date_dec", "date_dec_yearly", "date_dec_weekly", "Hour", "Temperature", "Humidity", "WindSpeed", "Visibility", 
+        "Visibility_2000_or_more", "DewPointTemperature", "SolarRadiation", "Rainfall", "Snowfall", "Seasons_Winter", "Seasons_Summer", "Seasons_Autumn", 
+        "Seasons_Spring", "Holiday", "FunctioningDay" };
       HEAL.EquationSearch.Console.Program.PrepareData(options, ref inputs, out _, out _, out _, out _, out _, out _, out _, out trainX, out trainY, out _);
     }
   }
